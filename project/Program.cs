0867f45a9fa0766fa21b3ac9affc76c5ff614163
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:16:41 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,31 @@
+﻿// Даны вещественные числа a и b. Определите, принадлежит ли точка с координатами (a; b) заштрихованной области;
+
+using System;
+
+class work_4_5
+{
+    static void Main()   
+    {
+        Console.Write("Введите a: "); 
+        float a = float.Parse(Console.ReadLine()); 
+        Console.Write("Введите b: "); 
+        float b = float.Parse(Console.ReadLine()); 
+
+        if (a >= -1 && a <= 3)  
+        {
+            if (b >= -2 && b <= 4) 
+            {
+                Console.WriteLine("Принадлежит"); 
+            }
+            else 
+            {
+                Console.WriteLine("Не принадлежит"); 
+            }       
+        }
+        else 
+        {
+            Console.WriteLine("Не принадлежит"); 
+        }
+    }
+}  
+ 
